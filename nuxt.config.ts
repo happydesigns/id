c2646ff11778e93649e97b798d6373dd2eb62207
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-06-24 20:25:41 +0200

    chore: expose consumer-safe nuxt layer config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,12 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { defineNuxtConfig } from 'nuxt/config'
-
-const currentDir = dirname(fileURLToPath(import.meta.url))
+import layerConfig from './nuxt.layer.config'
 
 export default defineNuxtConfig({
-  $meta: {
-    name: '@happydesigns/id'
-  },
+  ...layerConfig,
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
-
-  components: [
-    {
-      path: resolve(currentDir, './app/components'),
-      pathPrefix: false,
-      prefix: 'Id'
-    }
-  ],
-
-  css: [
-    resolve(currentDir, './app/assets/css/id.css')
+    ...(layerConfig.modules ?? []),
+    '@nuxt/eslint'
   ],
 
   compatibilityDate: 'latest'
---
+++ nuxt.layer.config.ts
@@ -0,0 +1,29 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineNuxtConfig } from 'nuxt/config'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  $meta: {
+    name: '@happydesigns/id'
+  },
+
+  modules: [
+    '@nuxt/ui'
+  ],
+
+  components: [
+    {
+      path: resolve(currentDir, './app/components'),
+      pathPrefix: false,
+      prefix: 'Id'
+    }
+  ],
+
+  css: [
+    resolve(currentDir, './app/assets/css/id.css')
+  ],
+
+  compatibilityDate: 'latest'
+})
