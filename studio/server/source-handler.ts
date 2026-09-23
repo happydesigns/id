8d3546da43d3ebc855d96cb5eb0a2267324b17f3
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2026-09-23 13:17:34 +0200

    fix(studio): use compiled core in source endpoint

--- studio/server/source-handler.ts
+++ studio/server/source-handler.ts
@@ -1,4 +1,4 @@
-import { studioDocumentMaxBytes } from '../../src/studio-document'
+import { studioDocumentMaxBytes } from '@happydesigns/id/studio/core'
 import { createError, defineEventHandler, getHeader, getMethod, getRequestURL, readBody } from 'h3'
 import type { H3Event } from 'h3'
 import { basename } from 'node:path'
